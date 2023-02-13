53bfc798928878cb357568afb265d36a14f81a4b
DIEGODEJESUSVASQUEZSANTOS <l191160152@pochutla.tecnm.mx>
2023-02-12 18:48:44 -0600

    Nombre del proyecto: my-app. Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/die/myapp/entity/TestEntity.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.die.myapp.entity;
+
+/**
+ * @author Diego de Jesus Vasquez Santos
+ *Propiedad de prueba para la muestra de compilacion del archivo testentity
+ */
+public class TestEntity {
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+}
